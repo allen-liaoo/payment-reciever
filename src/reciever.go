cfce7843087b5cfb781008925fa50ad651708ba1
Allen Liao <wcliaw610@gmail.com>
2025-02-23 20:54:35 -0600

    init

---
+++ src/reciever.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	var usdcAddr = os.Getenv("USDC_ADDRESS")
+	var myWalletAddr = os.Getenv("MY_WALLET_ADDRESS")
+	if usdcAddr == "" || myWalletAddr == "" {
+		panic("USDC_ADDRESS or MY_WALLET_ADDRESS environment variable is not set")
+	}
+	fee, err := getMaxPriorityFee()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(fee)
+
+	balance, err := getWalletBalance(myWalletAddr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(balance)
+
+	tokenBalance, err := getTokenBalance(usdcAddr, myWalletAddr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(tokenBalance)
+}
