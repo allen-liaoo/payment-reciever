6c82890c807a71d88b9df16bdb1e229f64c1d5c8
Allen Liao <wcliaw610@gmail.com>
2025-03-01 17:12:18 -0600

    local testing code; ran and got stuck waiting for first txt to be mined, and when running it again, got error: ALREADY_EXISTS: already known

--- reciever.go
+++ reciever.go
@@ -2,82 +2,173 @@ package main
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"fmt"
 	"math/big"
 	"os"
 
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/joho/godotenv"
 
 	wallet "allen-liaoo/payment-reciever/wallet"
 )
 
-func main() {
+var client *ethclient.Client
 
-	// grab env varaibles
+var usdcAddress common.Address
+var providerWalletAddress common.Address
+var providerWalletPK *ecdsa.PrivateKey
+var middlewareWalletMneumonic string
+var desAddress common.Address
+
+func init() {
 	godotenv.Load()
 	var rpcUrl = os.Getenv("RPC_URL")
 	var infuraKey = os.Getenv("INFURA_KEY")
 	if rpcUrl == "" || infuraKey == "" {
 		panic("RPC_URL or INFURA_KEY environment variable is not set")
 	}
-	var usdcAddr = os.Getenv("USDC_ADDRESS")
-	var myWalletAddr = os.Getenv("MY_WALLET_ADDRESS")
-	if usdcAddr == "" || myWalletAddr == "" {
-		panic("USDC_ADDRESS or MY_WALLET_ADDRESS environment variable is not set")
-	}
-	var usdcAddress = common.HexToAddress(usdcAddr)
-	var myWalletAddress = common.HexToAddress(myWalletAddr)
 
 	// init client
-	var client *ethclient.Client
-	client, err := ethclient.Dial(rpcUrl + infuraKey)
+	var err error
+	client, err = ethclient.Dial(rpcUrl + infuraKey)
 	if err != nil {
 		panic(err)
 	}
 
-	// get eth balance
-	balance, err := client.BalanceAt(context.Background(), myWalletAddress, nil)
+	var usdcAddr = os.Getenv("USDC_ADDRESS")
+	var providerWalletAddr = os.Getenv("PROVIDER_WALLET_ADDRESS")
+	var providerWalletPrivateKey = os.Getenv("PROVIDER_WALLET_PK")
+	middlewareWalletMneumonic = os.Getenv("MIDDLEWARE_MNEUMONIC")
+	if usdcAddr == "" || providerWalletAddr == "" || providerWalletPrivateKey == "" || middlewareWalletMneumonic == "" {
+		panic("USDC_ADDRESS or PROVIDER_WALLET_ADDRESS or PROVIDER_WALLET_PK or MIDDLEWARE_MNEUMONIC environment variable is not set")
+	}
+	usdcAddress = common.HexToAddress(usdcAddr)
+	providerWalletAddress = common.HexToAddress(providerWalletAddr)
+	providerWalletPK, err = crypto.HexToECDSA(providerWalletPrivateKey)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("balance (eth): ", balance, " (", wallet.FromRawUnit(balance, 18), ")")
+}
 
-	// Get USDC token balance
+func main() {
+	// TODO: Derive necessary middleware wallets
+	var testDerivationPath = "m/44'/60'/0'/0/5"
+	middlewareWallet, privateKey, err := wallet.DeriveWallet(middlewareWalletMneumonic, testDerivationPath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("middleware wallet address: ", middlewareWallet.Address.Hex())
+
+	balance, err := wallet.GetTokenBalance(client, usdcAddress, middlewareWallet.Address)
+	if err != nil {
+		panic(err)
+	}
 	decimals, err := wallet.GetContractDecimals(client, usdcAddress) // get contract decimals
 	if err != nil {
 		panic(err)
 	}
-	tokenBalance, err := wallet.GetTokenBalance(client, usdcAddress, myWalletAddress)
+	fmt.Println("middleware balance (usdc): ", balance, " (", wallet.FromRawUnit(balance, decimals), ")")
+
+	// TODO: Check if middleware wallet has expected balance to sweep
+	if balance.Cmp(big.NewInt(0)) < 0 {
+		panic("middleware wallet has not enough balance to sweep")
+	}
+
+	// Estimate cost to sweep, including
+	// fee (base + priority) of middleware wallet to destination wallet
+	// and fee of provider wallet to middleware wallet
+	// gas1, data1, err := wallet.EstimateGas(client, middlewareWallet.Address, desAddress, balance, true)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// gas1BI := new(big.Int).SetUint64(gas1)
+	// fmt.Println("gas from middleware to destination: ", gas1)
+	// gas2, data2, err := wallet.EstimateGas(client, providerWalletAddress, middlewareWallet.Address, gas1, false)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("gas from provider to middleware: ", gas2)
+
+	gastipcap, err := client.SuggestGasTipCap(context.Background())
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("balance (asdc):", tokenBalance, " (", wallet.FromRawUnit(tokenBalance, decimals), ")")
+	fmt.Println("gas tip cap: ", gastipcap)
 
-	// General gas price
-	gasPrice, err := client.SuggestGasPrice(context.Background())
+	// TODO: Check if gas cost is affordable
+	threshold := big.NewInt(0)
+	if threshold.Cmp(gastipcap) > 0 {
+		panic("gas tip cap is too high")
+	}
+
+	// sweep transaction
+	// 1. Transfer ETH gas fee from provider wallet to middleware wallet
+	tx1, err := wallet.SendTx(client, providerWalletAddress, middlewareWallet.Address, gastipcap, nil, gastipcap, nil, providerWalletPK)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("gas (general): ", gasPrice, " (", wallet.FromRawUnit(gasPrice, 18), ")")
+	fmt.Println("tx1: ", tx1)
+	fmt.Println("tx1 hash: ", tx1.Hash().Hex())
+	fmt.Println("waiting for tx1 to be mined...")
+	// _, err = bind.WaitMined(context.Background(), client, tx1)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	// Estimate gas for token transfer (From my wallet address to desAddress)
-	desAddress := common.HexToAddress("0x4d79b07f793FB42e7870c568cB374DBDc3BeBA51") // random address I grabbed from etherscan
-	preAmount := big.NewInt(1)
-	amount := wallet.ToRawUnit(preAmount, decimals)                           // amount with decimals
-	data := wallet.BuildTokenTxDataField(myWalletAddress, desAddress, amount) // data field for contract tokens transfer
-	msg := ethereum.CallMsg{                                                  // test transaction
-		From:  myWalletAddress,
-		To:    &desAddress,
-		Value: big.NewInt(0),
-		Data:  data,
-	}
-	gas, err := client.EstimateGas(context.Background(), msg)
+	// 2. Transfer USDC from middleware wallet to destination wallet
+	tx2, err := wallet.SendTokenTx(client, middlewareWallet.Address, desAddress, usdcAddress, balance, nil, gastipcap, privateKey)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("gas to", desAddress.Hex(), ":", gas)
-
+	fmt.Println("tx2: ", tx2)
+	fmt.Println("tx2 hash: ", tx2.Hash().Hex())
 }
+
+// func test() {
+
+// 	// get eth balance
+// 	balance, err := client.BalanceAt(context.Background(), providerWalletAddress, nil)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("balance (eth): ", balance, " (", wallet.FromRawUnit(balance, 18), ")")
+
+// 	// Get USDC token balance
+// 	decimals, err := wallet.GetContractDecimals(client, usdcAddress) // get contract decimals
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	tokenBalance, err := wallet.GetTokenBalance(client, usdcAddress, providerWalletAddress)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("balance (asdc):", tokenBalance, " (", wallet.FromRawUnit(tokenBalance, decimals), ")")
+
+// 	// General gas price
+// 	gasPrice, err := client.SuggestGasPrice(context.Background())
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("gas (general): ", gasPrice, " (", wallet.FromRawUnit(gasPrice, 18), ")")
+
+// 	// Estimate gas for token transfer (From my wallet address to desAddress)
+// 	desAddress := common.HexToAddress("0x4d79b07f793FB42e7870c568cB374DBDc3BeBA51") // random address I grabbed from etherscan
+// 	preAmount := big.NewInt(1)
+// 	amount := wallet.ToRawUnit(preAmount, decimals)                                 // amount with decimals
+// 	data := wallet.BuildTokenTxDataField(providerWalletAddress, desAddress, amount) // data field for contract tokens transfer
+// 	msg := ethereum.CallMsg{                                                        // test transaction
+// 		From:  providerWalletAddress,
+// 		To:    &desAddress,
+// 		Value: big.NewInt(0),
+// 		Data:  data,
+// 	}
+// 	gas, err := client.EstimateGas(context.Background(), msg)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("gas to", desAddress.Hex(), ":", gas)
+
+// }
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -9,17 +9,20 @@ import (
 	"math/big"
 	"os"
 
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
 	"golang.org/x/crypto/sha3"
 
 	contracts "allen-liaoo/payment-reciever/contracts"
 )
 
 type Contract struct {
-	name     string
-	decimals uint8
+	Name     string
+	Decimals uint8
 }
 
 var knownContracts map[string]Contract
@@ -41,7 +44,7 @@ func GetContractDecimals(client *ethclient.Client, contractAddress common.Addres
 
 	con, ok := knownContracts[contractAddress.Hex()]
 	if ok {
-		return con.decimals, nil
+		return con.Decimals, nil
 	}
 
 	// otherwise, lookup contract decimals
@@ -75,11 +78,28 @@ func GetTokenBalance(client *ethclient.Client, contractAddress common.Address, w
 	return balance, nil
 }
 
+// for pre EIP-1995 transactions
+func EstimateGas(client *ethclient.Client, from common.Address, to common.Address, amount *big.Int, isToken bool) (uint64, []byte, error) {
+	var rawAmount = ToRawUnit(amount, 18)
+	var data []byte = nil
+	if isToken {
+		data = BuildTokenTxDataField(to, rawAmount) // data field for contract tokens transfer
+	}
+	msg := ethereum.CallMsg{ // test transaction
+		From:  from,
+		To:    &to,
+		Value: amount, // value
+		Data:  data,
+	}
+	gas, err := client.EstimateGas(context.Background(), msg)
+	return gas, data, err
+}
+
 // display unit to raw unit
 func FromRawUnit(amount *big.Int, decimals uint8) *big.Int {
 	base := big.NewInt(10)
 	var newAmount big.Int
-	newAmount.Div(amount, base.Exp(base, big.NewInt(int64(decimals)), nil))
+	newAmount.Div(amount, base.Exp(base, new(big.Int).SetInt64(int64(decimals)), nil))
 	return &newAmount
 }
 
@@ -91,44 +111,58 @@ func ToRawUnit(amount *big.Int, decimals uint8) *big.Int {
 }
 
 // returns the hash of the transaction (in hex)
-func SendTx(client *ethclient.Client, from common.Address, to common.Address, amount *big.Int, gasLimit uint64, gasPrice *big.Int, data []byte, privateKey *ecdsa.PrivateKey) (string, error) {
+func SendTx(client *ethclient.Client, from common.Address, to common.Address, amount *big.Int, gasFeeCap *big.Int, gasTipCap *big.Int, data []byte, privateKey *ecdsa.PrivateKey) (*types.Transaction, error) {
 	nonce, err := client.PendingNonceAt(context.Background(), from)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-
-	tx := types.NewTransaction(
-		nonce,
-		to,
-		amount,
-		gasLimit,
-		gasPrice,
-		data,
-	)
-
 	chainID, err := client.NetworkID(context.Background())
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	// EIP1559 transaction
+	tx := types.NewTx(&types.DynamicFeeTx{
+		ChainID:   chainID,
+		Nonce:     nonce,
+		To:        &to,
+		Value:     amount,
+		GasFeeCap: gasFeeCap,
+		GasTipCap: big.NewInt(0),
+		Gas:       21000,
+		Data:      data,
+	})
+	signedTx, err := types.SignTx(tx, types.LatestSignerForChainID(chainID), privateKey)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-
 	err = client.SendTransaction(context.Background(), signedTx)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return signedTx.Hash().Hex(), nil
+	return signedTx, nil
+	// Legacy Transaction
+	// tx := types.NewTransaction(
+	// 	nonce,
+	// 	to,
+	// 	amount,
+	// 	gasLimit,
+	// 	gasPrice,
+	// 	data,
+	// )
+	// signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	// if err != nil {
+	// 	return "", err
+	// }
 }
 
-func SendTokenTx(client *ethclient.Client, from common.Address, contractAddress common.Address, gasLimit uint64, gasPrice *big.Int, data []byte, privateKey *ecdsa.PrivateKey) (string, error) {
-	return SendTx(client, from, contractAddress, big.NewInt(0), gasLimit, gasPrice, data, privateKey)
+func SendTokenTx(client *ethclient.Client, from common.Address, to common.Address, contractAddress common.Address, amount *big.Int, gasFeeCap *big.Int, gasTipCap *big.Int, privateKey *ecdsa.PrivateKey) (*types.Transaction, error) {
+	data := BuildTokenTxDataField(to, amount)
+	return SendTx(client, from, contractAddress, big.NewInt(0), gasFeeCap, gasTipCap, data, privateKey)
 }
 
-func BuildTokenTxDataField(contractAddress common.Address, to common.Address, amount *big.Int) []byte {
+func BuildTokenTxDataField(to common.Address, amount *big.Int) []byte {
 	transferFnSignature := []byte("transfer(address,uint256)")
 	hash := sha3.NewLegacyKeccak256()
 	hash.Write(transferFnSignature)
@@ -142,4 +176,25 @@ func BuildTokenTxDataField(contractAddress common.Address, to common.Address, am
 	data = append(data, paddedAddress...)
 	data = append(data, paddedAmount...)
 	return data
+}
+
+// DeriveWallet derive wallet from mnemonic and path. It returns the account and private key.
+func DeriveWallet(mnemonic string, path string) (*accounts.Account, *ecdsa.PrivateKey, error) {
+	derPath, err := hdwallet.ParseDerivationPath(path)
+	if err != nil {
+		return nil, nil, err
+	}
+	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
+	if err != nil {
+		return nil, nil, err
+	}
+	account, err := wallet.Derive(derPath, false)
+	if err != nil {
+		return nil, nil, err
+	}
+	privateKey, err := wallet.PrivateKey(account)
+	if err != nil {
+		return nil, nil, err
+	}
+	return &account, privateKey, nil
 }
